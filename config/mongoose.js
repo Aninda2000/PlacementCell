c265254362e78abfc5b64cf5e5dc95d12f61d1da
Aninda2000 <anindaroy100@gmail.com>
2022-07-20 13:23:37 +0530

    changes in port

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://0.0.0.0:27017/Placement-Cell');
+mongoose.connect('mongodb://localhost:27017/Placement-Cell');
 
 const db = mongoose.connection;
 
