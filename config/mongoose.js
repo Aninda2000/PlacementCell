30f43fbe1eaaefd8168a25831c879314ccbf89fa
Aninda2000 <anindaroy100@gmail.com>
2022-07-21 20:45:17 +0530

    add mongodb atlas

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/Placement-Cell');
+// mongoose.connect('mongodb://localhost:27017/Placement-Cell');
+mongoose.connect('mongodb+srv://aninda:aninda123@cluster0.oq73w.mongodb.net/?retryWrites=true&w=majority');
+
 
 const db = mongoose.connection;
 
